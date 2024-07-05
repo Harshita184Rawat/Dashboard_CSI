da563f8a37b49e0ac3cdf7a15b8ca44b526471e7
Harshita184Rawat <harshita.reenu184@gmail.com>
2024-07-05 10:37:32 +0530

    Sparkline and Stacked Component updated

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -4,7 +4,27 @@ import { SparklineTooltip, Inject, SparklineComponent } from '@syncfusion/ej2-re
 const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
     {/* Syncfusion documentation to get info about the props use in the components */ }
     return (
-        < SparklineComponent >
+        < SparklineComponent
+            id={id}
+            height={height}
+            width={width}
+            lineWidth={1}
+            valueType="Numeric"
+            fill={color}
+            border={{ color: currentColor, width: 2 }}
+            dataSource={data}
+            xName='x'
+            yName='y'
+            tooltipSettings={{
+                visible: true,
+                format: '${x} : data ${y}',
+                trackLineSettings: {
+                    visible: true
+                }
+            }}
+
+        >
+            <Inject services={[SparklineTooltip]} />
 
         </SparklineComponent >
     )
